530ad9870e50d0afbf996d72f0fe6ec081be1c2e
Enmin Zhou <zhouem14@gmail.com>
2019-11-02 15:52:48 -0700

    fix query bugs

--- algorithm/trial.py
+++ algorithm/trial.py
@@ -14,29 +14,25 @@ def topoSort(net):
     variableList = []
     while len(variablesSet) < len(variables):
         for variable in variables:
-            if variable not in variablesSet and all(parent in variablesSet for parent in net[variable]['parents']):
-                # add the variable `v` into the set `s` iff
-                # all parents of `v` are already in `s`.
+            if variable not in variablesSet and all(
+                    parent in variablesSet
+                    for parent in net[variable]['parents']):
                 variablesSet.add(variable)
                 variableList.append(variable)
     return variableList
 
 
 def queryGiven(net, Y, e):
-        if net[Y]['prob'] != -1:
-            prob = net[Y]['prob'] if e[Y] else 1 - net[Y]['prob']
-
-        # Y has at least 1 parent
-        else:
-            # get the value of parents of Y
-            parents = tuple(e[p] for p in net[Y]['parents'])
-
-            # query for prob of Y = y
-            prob = net[Y]['condprob'][parents] if e[Y] else 1 - \
-                                                                 net[Y][
-                                                                     'condprob'][
-                                                                     parents]
-        return prob
+    if net[Y]['prob'] != -1:
+        prob = net[Y]['prob'] if e[Y] else 1 - net[Y]['prob']
+    # Y has at least 1 parent
+    else:
+        # get the value of parents of Y
+        parents = tuple(e[p] for p in net[Y]['parents'])
+        # query for prob of Y = y
+        prob = net[Y]['condprob'][parents] if e[
+            Y] else 1 - net[Y]['condprob'][parents]
+    return prob
 
 
 def genPermutations(length):
@@ -46,25 +42,24 @@ def genPermutations(length):
         return permutationsmemo[length]
     else:
         perms = set()
-        for comb in itertools.combinations_with_replacement([False, True],length):
+        for comb in itertools.combinations_with_replacement([False, True],
+                                                            length):
             for perm in itertools.permutations(comb):
                 perms.add(perm)
         perms = list(perms)
         # perms = [(False, False, False), (False, False, True), ...]
         assert (len(perms) == pow(2, length))
+
         permutationsmemo[length] = perms
-    return perms
+        return perms
 
 
 def makeFactor(net, var, factorvars, e):
     variables = factorvars[var]
     variables.sort()
-
     allvars = copy.deepcopy(net[var]['parents'])
     allvars.append(var)
-
     perms = genPermutations(len(allvars))
-
     entries = {}
     asg = {}
     for perm in perms:
@@ -83,7 +78,7 @@ def makeFactor(net, var, factorvars, e):
     return (variables, entries)
 
 
-def pointwise(var, factor1, factor2):
+def pointwise(factor1, factor2):
     newvariables = []
     newvariables.extend(factor1[0])
     newvariables.extend(factor2[0])
@@ -101,43 +96,49 @@ def pointwise(var, factor1, factor2):
         key2 = tuple(asg[v] for v in factor2[0])
         prob = factor1[1][key1] * factor2[1][key2]
         newtable[key] = prob
+
     return (newvariables, newtable)
 
 
 def sumOut(var, factors):
     pwfactors = []  # list of factors containing var
     indices = []
-    for i, factor in enumerate(factors):
+    for fIndex, factor in enumerate(factors):
         if var in factor[0]:
             pwfactors.append(factor)
-            indices.append(i)
+            indices.append(fIndex)
     if len(pwfactors) > 1:
-        for i in reversed(indices):
-            del factors[i]
+        for index in reversed(indices):
+            del factors[index]
         result = pwfactors[0]
         for factor in pwfactors[1:]:
-            result = pointwise(var, result, factor)
+            result = pointwise(result, factor)
         factors.append(result)
 
-    for i, factor in enumerate(factors):
-        for j, v in enumerate(factor[0]):
+    for fIndex, factor in enumerate(factors):
+        for vIndex, v in enumerate(factor[0]):
+            # if the variable is hidden
             if v == var:
-                newvariables = factor[0][:j] + factor[0][j + 1:]
-                newentries = {}
+                # variable list of the new factor
+                newVariables = factor[0][:vIndex] + factor[0][vIndex + 1:]
+                # probability table of the new factor
+                newEntries = {}
                 for entry in factor[1]:
                     entry = list(entry)
-                    newkey = tuple(entry[:j] + entry[j + 1:])
+                    newkey = tuple(entry[:vIndex] + entry[vIndex + 1:])
 
-                    entry[j] = True
+                    entry[vIndex] = True
                     prob1 = factor[1][tuple(entry)]
-                    entry[j] = False
+                    entry[vIndex] = False
                     prob2 = factor[1][tuple(entry)]
                     prob = prob1 + prob2
 
-                    newentries[newkey] = prob
-                factors[i] = (newvariables, newentries)
-                if len(newvariables) == 0:
-                    del factors[i]
+                    newEntries[newkey] = prob
+
+                # replace the old factor
+                factors[fIndex] = (newVariables, newEntries)
+                if len(newVariables) == 0:
+                    del factors[fIndex]
     return factors
 
 
@@ -146,13 +147,9 @@ def enum_ask(net, X, e):
     for x in [False, True]:
         e = copy.deepcopy(e)
         e[X] = x
-
         variables = topoSort(net)
-
-            # enumerate
+        # enumerate
         dist.append(enum_all(net, variables, e))
-
-        # normalize & return
     return normalize(dist)
 
 
@@ -170,41 +167,31 @@ def enum_all(net, variables, e):
             probs.append(
                 queryGiven(net, Y, e2) * enum_all(net, variables[1:], e2))
         ret = sum(probs)
-
-    print("%-14s | %-20s = %.8f" % (
-        ' '.join(variables),
-        ' '.join('%s=%s' % (v, 't' if e[v] else 'f') for v in e),
-        ret
-    ))
+    print("%-14s | %-20s = %.8f" % (' '.join(variables), ' '.join(
+        '%s=%s' % (v, 't' if e[v] else 'f') for v in e), ret))
     return ret
 
 
 def elim_ask(net, X, e):
     eliminated = set()
     factors = []
-
     while len(eliminated) < len(net):
-        variables = filter(lambda v: v not in eliminated,
-                            list(net.keys()))
-
+        variables = filter(lambda v: v not in eliminated, list(net.keys()))
         variables = filter(
-            lambda v: all(c in eliminated for c in net[v]['children']),variables)
-
+            lambda v: all(c in eliminated for c in net[v]['children']),
+            variables)
         factorvars = {}
         for v in variables:
-            factorvars[v] = [p for p in net[v]['parents'] if p not in e]  # and p != X]
-            if v not in e:  # and v != X:
+            factorvars[v] = [p for p in net[v]['parents'] if p not in e]
+            if v not in e:
                 factorvars[v].append(v)
-
-        var = sorted(factorvars.keys(), key=(lambda x: (len(factorvars[x]), x)))[0]
+        var = sorted(factorvars.keys(),
+                     key=(lambda x: (len(factorvars[x]), x)))[0]
         print('----- Variable: %s -----' % var)
-
         if len(factorvars[var]) > 0:
-            factors.append(makeFactor(var, factorvars, e, net))
-
+            factors.append(makeFactor(net, var, factorvars, e))
         if var != X and var not in e:
             factors = sumOut(var, factors)
-
         eliminated.add(var)
         print('Factors:')
         for factor in factors:
@@ -215,71 +202,44 @@ def elim_ask(net, X, e):
                 for pair in zip(factor[0], perm):
                     asg[pair[0]] = pair[1]
                 key = tuple(asg[v] for v in factor[0])
-                print('%s: %.4f' % (
-                    ' '.join('%s=%s' % (k, 't' if asg[k] else 'f') for k in
-                             sorted(asg.keys())),
-                    factor[1][key]
-                ))
-            print()
-
+                print('%s: %.4f' %
+                      (' '.join('%s=%s' % (k, 't' if asg[k] else 'f')
+                                for k in sorted(asg.keys())), factor[1][key]))
     if len(factors) >= 2:
         result = factors[0]
         for factor in factors[1:]:
-            result = pointwise(var, result, factor)
+            result = pointwise(result, factor)
     else:
         result = factors[0]
     return normalize((result[1][(False,)], result[1][(True,)]))
 
 
-def query(fname, alg, q):
+def query(fname, method, query):
 
-    # construct the net from the given file name
     try:
         net = parse.buildNet(fname)
     except:
         print('Failed to parse %s' % fname)
         exit()
 
-    # parse the given query
-    match = re.match(r'P\((.*)\|(.*)\)', q)
+    match = re.match(r'P\((.*)\|(.*)\)', query)
     if match:
         X = match.group(1).strip()
         e = match.group(2).strip().split(',')
         edict = dict((x[0], True if x[2] == 't' else False) for x in e)
     else:
-        match = re.match(r'P\((.*)\)', q)
+        match = re.match(r'P\((.*)\)', query)
         X = match.group(1).strip()
         e = []
         edict = {}
 
-    # call the appropriate function
-    dist = enum_ask(net, X, edict) if alg == 'enum' else elim_ask(net, X, edict)
+    dist = enum_ask(net, X, edict) if method == 'enum' else elim_ask(net, X, edict)
     print("\nRESULT:")
     for prob, x in zip(dist, [False, True]):
-        print("P(%s = %s | %s) = %.4f" %
-              (X,
-               't' if x else 'f',
-               ', '.join('%s = %s' % tuple(v.split('=')) for v in e),
-               prob))
-
-
-def main():
-    try:
-        fname = sys.argv[1]
-
-        alg = sys.argv[2]
-        assert (alg == 'enum' or alg == 'elim')
-
-        q = sys.argv[3]
-    except SyntaxError:
-        print('Invalid syntax for bayes net file %s' % sys.argv[1])
-    except IndexError:
-        print('Not enough argument.')
-        print('Usage: %s <bayesnet> <enum|elim> <query>' % sys.argv[0])
-    query(fname, alg, q)
+        print("P(%s = %s | %s) = %.4f" % (X, 't' if x else 'f', ', '.join(
+            '%s = %s' % tuple(v.split('=')) for v in e), prob))
 
 
 if __name__ == '__main__':
-    # import doctest
-    # doctest.testmod()
-    query('/home/enminz/Research/Bayesian Network/unittest/testGraphs/alarm.bn', 'enum', 'A')
+    query('/home/enminz/Research/Bayesian Network/unittest/testGraphs/alarm.bn',
+          'elim', 'P(B|J=t,M=t)')
--- unittest/testTrial.py
+++ unittest/testTrial.py
@@ -7,6 +7,7 @@ import algorithm.parse as parse
 
 
 class TestBayesNet(unittest.TestCase):
+
     def setUp(self):
         self.net_alarm = parse.buildNet(os.path.join('testGraphs', 'alarm.bn'))
         self.net_ex2 = parse.buildNet(os.path.join('testGraphs', 'ex2.bn'))
@@ -33,27 +34,52 @@ class TestBayesNet(unittest.TestCase):
         self.assertEqual(res, ['A', 'B', 'C', 'D', 'E'])
 
     def test_querygiven_alarm(self):
-        cases = [
-            (('B', {'B': False}), .999),
-            (('B', {'B': True}), .001),
-            (('A', {'A': True, 'B': True, 'E': False}), .94),
-            (('A', {'A': False, 'B': False, 'E': True}), .71),
-            (('A', {'A': False, 'B': False, 'E': False}), .999),
-            (('M', {'M': False, 'A': False}), .99)
-        ]
+        cases = [(('B', {
+            'B': False
+        }), .999), (('B', {
+            'B': True
+        }), .001), (('A', {
+            'A': True,
+            'B': True,
+            'E': False
+        }), .94), (('A', {
+            'A': False,
+            'B': False,
+            'E': True
+        }), .71), (('A', {
+            'A': False,
+            'B': False,
+            'E': False
+        }), .999), (('M', {
+            'M': False,
+            'A': False
+        }), .99)]
         for i, o in cases:
             self.assertAlmostEqual(trial.queryGiven(self.net_alarm, *i), o)
 
     def test_querygiven_ex2(self):
-        cases = [
-            (('A', {'A': False}), 0.7),
-            (('B', {'B': True}), 0.6),
-            (('E', {'C': True, 'E': False}), 0.3),
-            (('D', {'B': False, 'A': True, 'D': False}), 0.2),
-            (('D', {'B': False, 'A': True, 'D': True}), 0.8),
-            (('C', {'C': True, 'A': True}), 0.8),
-            (('C', {'C': False, 'A': False}), 0.6)
-        ]
+        cases = [(('A', {
+            'A': False
+        }), 0.7), (('B', {
+            'B': True
+        }), 0.6), (('E', {
+            'C': True,
+            'E': False
+        }), 0.3), (('D', {
+            'B': False,
+            'A': True,
+            'D': False
+        }), 0.2), (('D', {
+            'B': False,
+            'A': True,
+            'D': True
+        }), 0.8), (('C', {
+            'C': True,
+            'A': True
+        }), 0.8), (('C', {
+            'C': False,
+            'A': False
+        }), 0.6)]
         for i, o in cases:
             self.assertAlmostEqual(trial.queryGiven(self.net_ex2, *i), o)
 
@@ -67,45 +93,76 @@ class TestBayesNet(unittest.TestCase):
 
     def test_makeFactor0(self):
         i = ('D', {'D': ['D', 'A']}, {'B': True})
-        o = (['A', 'D'], {(True, True): 0.7, (True, False): 0.30000000000000004,
-            (False, True): 0.1, (False, False): 0.9})
+        o = (['A', 'D'], {
+            (True, True): 0.7,
+            (True, False): 0.30000000000000004,
+            (False, True): 0.1,
+            (False, False): 0.9
+        })
         self.assertEqual(trial.makeFactor(self.net_ex2, *i), o)
 
     def test_pointwise0(self):
-        i1 = (['C', 'E'], {(False, False): 0.8, (False, True): 0.2, (True, True): 0.7, (True, False): 0.3})
-        i2 = (['A', 'C'], {(True, True): 0.8, (True, False): 0.2, (False, True): 0.4, (False, False): 0.6})
-        o = (['A', 'C', 'E'], {(False, True, True): 0.27999999999999997,
-            (False, False, False): 0.48, (True, True, False): 0.24,
-            (True, False, False): 0.16000000000000003, (False, True, False): 0.12,
-            (False, False, True): 0.12, (True, True, True): 0.5599999999999999,
-            (True, False, True): 0.04000000000000001})
-        self.assertEqual(trial.pointwise('C', i1, i2), o)
+        i1 = (['C', 'E'], {
+            (False, False): 0.8,
+            (False, True): 0.2,
+            (True, True): 0.7,
+            (True, False): 0.3
+        })
+        i2 = (['A', 'C'], {
+            (True, True): 0.8,
+            (True, False): 0.2,
+            (False, True): 0.4,
+            (False, False): 0.6
+        })
+        o = (['A', 'C', 'E'], {
+            (False, True, True): 0.27999999999999997,
+            (False, False, False): 0.48,
+            (True, True, False): 0.24,
+            (True, False, False): 0.16000000000000003,
+            (False, True, False): 0.12,
+            (False, False, True): 0.12,
+            (True, True, True): 0.5599999999999999,
+            (True, False, True): 0.04000000000000001
+        })
+        self.assertEqual(trial.pointwise(i1, i2), o)
 
     def test_sumOut0(self):
-        self.assertEqual(trial.sumOut('D', [(['A', 'D'], {(True, True): 0.7,
-            (True, False): 0.3, (False, True): 0.1, (False, False): 0.9})]),
-        [(['A'], {(False,): 1.0, (True,): 1.0})])
+        self.assertEqual(
+            trial.sumOut('D', [(['A', 'D'], {
+                (True, True): 0.7,
+                (True, False): 0.3,
+                (False, True): 0.1,
+                (False, False): 0.9
+            })]), [(['A'], {
+                (False,): 1.0,
+                (True,): 1.0
+            })])
 
     def test_alarm_ask1(self):
-        inputs = [
-            ('B', {'J': False, 'M': True}),
-            ('A', {'B': True, 'E': False, 'J': True, 'M': True}),
-            ('M', {'B': False, 'E': False}),
-            ('E', {'B': True}),
-            ('E', {'A': True, 'M': False})
-        ]
-        outputs = [
-            (0.9931, 0.0069),
-            (0.0001, 0.9999),
-            (0.9893, 0.0107),
-            (0.9980, 0.0020),
-            (0.7690, 0.2310)
-        ]
+        inputs = [('B', {
+            'J': False,
+            'M': True
+        }), ('A', {
+            'B': True,
+            'E': False,
+            'J': True,
+            'M': True
+        }), ('M', {
+            'B': False,
+            'E': False
+        }), ('E', {
+            'B': True
+        }), ('E', {
+            'A': True,
+            'M': False
+        })]
+        outputs = [(0.9931, 0.0069), (0.0001, 0.9999), (0.9893, 0.0107),
+                   (0.9980, 0.0020), (0.7690, 0.2310)]
         for i, i1 in enumerate(inputs):
             res = trial.enum_ask(self.net_alarm, *i1)
             self.assertEqual(round(res[0] - outputs[i][0], 3), 0)
             self.assertEqual(round(res[1] - outputs[i][1], 3), 0)
-            res = trial.enum_ask(self.net_alarm, *i1)
+            res = trial.elim_ask(self.net_alarm, *i1)
             self.assertEqual(round(res[0] - outputs[i][0], 3), 0)
             self.assertEqual(round(res[1] - outputs[i][1], 3), 0)
 
